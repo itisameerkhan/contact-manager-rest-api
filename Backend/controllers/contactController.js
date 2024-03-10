163487543910653db7fe0a1f6f78aa279adef45d
itisameerkhan <itisameerkhan@gmail.com>
2024-03-10 18:15:41 +0530

    error handler initiated

--- Backend/controllers/contactController.js
+++ Backend/controllers/contactController.js
@@ -1,57 +1,59 @@
+import asyncHandler from "express-async-handler";
+
 /*
  * @desc GET all contacts
  * @route GET /api/contacts
  * @access PUBLIC
  */
-export const getContacts = (req, res) => {
+export const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ sucess: true, message: "GET method" });
-};
+});
 
 /*
  * @desc GET single contact with Id
  * @route GET /api/contacts/:id
  * @access PUBLIC
  */
-export const getContactWithId = (req, res) => {
+export const getContactWithId = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ sucess: true, message: `GET method for ID ${req.params.id}` });
-};
+});
 
 /*
  * @desc CREATE new contact
  * @route POST /api/contacts
  * @access PUBLIC
  */
-export const createContact = (req, res) => {
+export const createContact = asyncHandler(async (req, res) => {
   console.log(req.body);
   const { name, email, phone } = req.body;
 
-  if(!name || !email || !phone) {
+  if (!name || !email || !phone) {
     res.status(400);
-    throw new Error("missing field")
+    throw new Error("missing field");
   }
   res.status(201).json({ sucess: true, message: "POST method" });
-};
+});
 
 /*
  * @desc UPDATE existing contact with id
  * @route PUT /api/contacts/:id
  * @access PUBLIC
  */
-export const updateContact = (req, res) => {
+export const updateContact = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ sucess: true, message: `PUT method ${req.params.id}` });
-};
+});
 
 /*
  * @desc DELETE existing contact with id
  * @route DELETE /api/contacts/:id
  * @access PUBLIC
  */
-export const deleteContact = (req, res) => {
+export const deleteContact = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ sucess: true, message: `DELETE method ${req.params.id}` });
-};
+});
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log("server listening on PORT ->", process.env.PORT);
